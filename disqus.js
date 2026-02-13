1266bbae7c63e8f327db37fdcf0626eef4b41ae3
Sanghyun Han <sanghyunhan@Sanghyun-MacBook-Pro.local>
2026-02-13 12:04:47 +0900

    fix: Correct Disqus shortname in disqus.js

--- disqus.js
+++ disqus.js
@@ -5,7 +5,7 @@ var disqus_config = function () {
 
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://https-ai-product-builder-v1-pages-dev-1.disqus.com/embed.js';
+    s.src = 'https://ai-product-builder-v1-pages-dev-1.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
